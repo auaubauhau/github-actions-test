6c04c07859614eb6061f4e98fd578bdcc269547c
Byeonggeol Ha <byeonggeol98@gmail.com>
2020-05-12 12:27:24 +0900

    마크다운 빌드 테스트

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 // Created by Byeonggeol Ha on 2020-05-12
 import * as React from "react";
+import * as ReactDOM from "react-dom";
+import * as showdown from "showdown";
+import * as content from "./page/main.md";
 
-interface Props {
-
-}
 
 export const Main = () => {
-    return <div>HI</div>
+    const converter = new showdown.Converter();
+    return (
+        <div dangerouslySetInnerHTML={{__html: converter.makeHtml(content)}}/>
+    );
 }
+
+ReactDOM.render(<Main/>, document.getElementById('root'));
---
+++ src/types/asset.d.ts
@@ -0,0 +1,12 @@
+// Created by Byeonggeol Ha on 2020-05-12
+declare module '*.png';
+declare module '*.ico';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
+declare module '*.svg';
+declare module '*.woff';
+declare module '*.woff2';
+declare module '*.ttf';
+declare module '*.eof';
+declare module '*.md';
